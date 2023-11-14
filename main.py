023a83da93718c035c0bc8d7ba9b2688da1b95f9
sayu349 <106569098+sayu349@users.noreply.github.com>
2023-11-14 15:25:04 +0900

    顧客情報入力完了ページを作成しました。

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import os
 
 app = Flask(__name__)
 
@@ -27,14 +28,22 @@ def customer_page():
 @app.route("/add_customer", methods=["GET","POST"])
 def add_customer_page():
     form = UserInfoForm(request.form)
-
     # POST
     if request.method == "POST":
-        return render_template("success.html")
+        # お名前
+        name = form.name.data
+        # 会社名
+        company = form.company.data
+        # お電話番号
+        tel = form.tel.data
+        # メールアドレス
+        email = form.email.data
+
+        return render_template("success.html",name=name,company=company,tel=tel,email=email)
     # GET
     else:
         return render_template("add_customer.html", form=form)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
