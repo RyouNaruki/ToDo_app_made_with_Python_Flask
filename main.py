d4585532be2b62d98dbe3f45fa92b6f48cd32810
sayu349 <106569098+sayu349@users.noreply.github.com>
2023-11-14 15:41:39 +0900

    データベースへの書き込み機能を追加しました。

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import os
+import sqlite3
 
 app = Flask(__name__)
 
 
 from forms import UserInfoForm
+
+
+# データベースにデータを追加する関数
+def add_data_to_database(name,company,tel,email):
+    # databaseにレコードを追加
+    con = sqlite3.connect('database/customer.db')
+    cur = con.cursor()
+    cur.execute('INSERT INTO customer (name, company, tel, email) VALUES (?, ?, ?, ?)',
+                (name, company, tel, email))
+    con.commit()
+    con.close()
 
 
 @app.route("/")
@@ -38,8 +50,31 @@ def add_customer_page():
         tel = form.tel.data
         # メールアドレス
         email = form.email.data
+        # 実行環境で条件分岐をする
+        if os.getenv('GAE_ENV', '').startswith('standard'): # True = cloud, False = local
+            # クラウド環境の場合
+            from google.cloud import storage
+            # GCS上のdbを取得する
+            client = storage.Client()
+            bucket_name = "todo-app-405104.appspot.com"
+            bucket = client.get_bucket(bucket_name)
+            blob_name = "customer.db"
+            blob = bucket.blob(blob_name)
+            blob.download_to_filename('/tmp/customer.db')
 
-        return render_template("success.html",name=name,company=company,tel=tel,email=email)
+            # データベースにデータを追加する
+            add_data_to_database(name,company,tel,email)
+
+            #dbを上書きする
+            blob.upload_from_filename('/tmp/customer.db')
+
+            return render_template("success.html",name=name,company=company,tel=tel,email=email)
+        else:
+            # ローカル環境の場合
+            # データベースにデータを追加する
+            add_data_to_database(name,company,tel,email)
+            return render_template("success.html",name=name,company=company,tel=tel,email=email)
+
     # GET
     else:
         return render_template("add_customer.html", form=form)
