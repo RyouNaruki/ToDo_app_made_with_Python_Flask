25c0b6efb2efbaea6c2d0501ff4c9aee1efc8d21
sayu349 <106569098+sayu349@users.noreply.github.com>
2023-11-14 15:07:46 +0900

    顧客追加フォームを追加しました。

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from flask import Flask
 from flask import render_template
-
+from flask import request
 
 app = Flask(__name__)
+
+
+from forms import UserInfoForm
 
 
 @app.route("/")
 def top_page():
     return render_template("top.html")
 
-
 @app.route("/customer_list")
 def customer_list_page():
     return render_template("customer_list.html")
-
 
 @app.route("/task")
 def task_page():
@@ -23,9 +24,18 @@ def task_page():
 def customer_page():
     return render_template("customer.html")
 
-@app.route("/add_customer")
+@app.route("/add_customer", methods=["GET","POST"])
 def add_customer_page():
-    return render_template("add_customer.html")
+    form = UserInfoForm(request.form)
 
+    # POST
+    if request.method == "POST":
+        return render_template("success.html")
+    # GET
+    else:
+        return render_template("add_customer.html", form=form)
+
+"""
 if __name__ == "__main__":
     app.run(debug=True)
+"""
