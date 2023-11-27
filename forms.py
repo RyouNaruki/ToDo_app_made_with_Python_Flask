7a7e7c989c40494959b538f5f221f229bb5fbc21
ryou-naruki <114968762+ryou-naruki@users.noreply.github.com>
2023-11-27 13:54:51 +0900

    update telfiedls

--- forms.py
+++ forms.py
@@ -1,12 +1,15 @@
 from wtforms import Form
+#電話番号のフィールドのインポート
+from wtforms.fields import TelField
 
 from wtforms.fields import (
     StringField, SubmitField
 )
 # python3.11以上の場合
-from wtforms.fields.html5 import EmailField
+#from wtforms.fields.html5 import EmailField
+#wtforms.fields.html5.TelField
 #python 3.11未満の場合
-#from wtforms.fields import EmailField
+from wtforms.fields import EmailField
 
 from wtforms.validators import (
     DataRequired, Email, EqualTo
@@ -15,14 +18,15 @@ from wtforms.validators import (
 
 # 顧客追加フォーム
 class AddCustomerForm(Form):
-    # 年齢：静数値入力
+    # 会社名：文字列入力
     company = StringField("会社名", render_kw={"placeholder":"〇〇〇〇株式会社"})
     # お電話番号：文字列入力
-    tel = StringField("電話番号",render_kw={"placeholder":"012-3456-7890"})
+    tel  = TelField("電話番号",render_kw={"placeholder":"012-3456-7890"})
     # メールアドレス：メールアドレス入力
     email = EmailField("メールアドレス", render_kw={"placeholder":"xxxx@example.com"}, validators=[EqualTo("confirm_password", "メールアドレスが一致しません")])
     # メールアドレス(確認用)：メールアドレス入力
     confirm_email = EmailField("メールアドレス確認", render_kw={"placeholder":"xxxx@example.com"}, validators=[Email("メールアドレスのフォーマットではありません")])
+    
     # ボタン
     submit = SubmitField("送信")
 
