b28e7fccf6224ff99ed7db22f75a06cf51f3876f
ryou-naruki <114968762+ryou-naruki@users.noreply.github.com>
2023-11-27 16:55:58 +0900

    タスク追加の諸々完了

--- forms.py
+++ forms.py
@@ -3,7 +3,7 @@ from wtforms import Form
 from wtforms.fields import TelField
 
 from wtforms.fields import (
-    StringField, SubmitField
+    StringField, SubmitField,IntegerField,DateField,SelectField
 )
 # python3.11以上の場合
 #from wtforms.fields.html5 import EmailField
@@ -31,5 +31,30 @@ class AddCustomerForm(Form):
     submit = SubmitField("送信")
 
 # ▼▼▼タスク追加フォームの型を作ってみよう▼▼▼
+class AddTaskForm(Form):
+    #会社の情報を引っ張ってくる（後々制作）：数値入力
+    customer_id = IntegerField("顧客ID", render_kw={"placeholder":"〇〇〇〇"})
+    #相手方担当者：文字列入力
+    sir = StringField("先方の担当者様", render_kw={"placeholder":"山田太郎"})
+    #タスク説明：文字列入力
+    task_content = StringField("タスクの内容", render_kw={"placeholder":"届出"})
+    #期日：日付入力
+    deadline = DateField("期日", render_kw={"placeholder":"2023-12-31"})
+    #担当者：セレクトボックス, PIC=Person In Change
+    pic = SelectField("担当者：　", choices=[
+                                            ("稲垣大輔"),
+                                            ("関幸子"), 
+                                            ("濱野竜聖"),
+                                            ("成木涼雨"),
+                                            ("宇佐美ささ波")
+                                            ])
+    #タスクの進捗状況：セレクトボックス
+    progress = SelectField("タスクの進捗状況：　", choices=[
+                                                        ( "未着手"),
+                                                        ("進行中"), 
+                                                        ("完了")
+                                                        ])
 
+    # ボタン
+    submit = SubmitField("送信")
 # ▲▲▲タスク追加フォームの型を作ってみよう▲▲▲
